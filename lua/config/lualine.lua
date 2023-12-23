f1e69740b5a2ba65e33cafd2a8e606c71cf0ca06
Blackhawk <daniel.peters1@pm.me>
2023-12-23 19:29:52 +0100

    Add time to statusline

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -1,3 +1,19 @@
+local function get_time()
+	return " " .. os.date("%H:%M") .. " "
+end
+
 require("lualine").setup({
-	options = { theme = "onedark" },
+	options = {
+		theme = "auto",
+		globalstatus = true,
+	},
+	sections = {
+		lualine_a = {"mode"},
+		lualine_b = {"branch", "diff", "diagnostics"},
+		lualine_c = {"filename"},
+		lualine_x = {"encoding", "fileformat", "filetype"},
+		lualine_y = {"progress", "location" },
+		lualine_z = { get_time },
+	}
 })
+
