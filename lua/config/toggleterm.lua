93d74795670810d0c97f6f70d2ecdac51b750c2a
Blackhawk <daniel.peters1@pm.me>
2023-12-26 22:07:07 +0100

    Add keybind for floating terminal

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -1,5 +1,6 @@
 require("toggleterm").setup{
-	open_mapping = [[<leader>t]],
+	open_mapping = [[<leader>tt]],
 	terminal_mappings = true,
 }
 
+vim.keymap.set("n", "<leader>tf", ":ToggleTerm direction=float<cr>")
