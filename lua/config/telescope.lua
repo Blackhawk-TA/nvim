d4929ee145ede4686da36601cd8dca56884fa6ee
Blackhawk <daniel.peters1@pm.me>
2023-12-26 21:54:05 +0100

    Update keybinds for telescope

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -6,7 +6,8 @@ require("telescope").setup{
 				-- map actions.which_key to <C-h> (default: <C-/>)
 				-- actions.which_key shows the mappings for your picker,
 				-- e.g. git_{create, delete, ...}_branch for the git_branches picker
-				["<C-h>"] = "which_key"
+				["<C-h>"] = "which_key",
+				["<C-s>"] = "select_vertical",
 			}
 		}
 	},
