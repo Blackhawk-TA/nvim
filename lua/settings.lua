944ccf056c0698e239d0987c444fde472d8f8d6f
Blackhawk <daniel.peters1@pm.me>
2023-12-26 20:49:43 +0100

    Add todo comment highlighting

--- lua/settings.lua
+++ lua/settings.lua
@@ -50,6 +50,7 @@ vim.o.undofile = true
 vim.o.autowrite = true
 
 -- Spellcheck
+vim.o.syntax = "enable"
 vim.o.spelllang = "en,de"
 vim.o.spell = true
 vim.api.nvim_create_autocmd("TermOpen", { pattern = "term://*", command = "setlocal nospell" }) -- Disable spellcheck in terminal
