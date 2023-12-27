69e3e155a743542c020ad7321504812169444612
Blackhawk <daniel.peters1@pm.me>
2023-12-27 16:39:22 +0100

    Give float terminal own instance

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -3,4 +3,4 @@ require("toggleterm").setup{
 	terminal_mappings = true,
 }
 
-vim.keymap.set("n", "<leader>tf", ":ToggleTerm direction=float<cr>")
+vim.keymap.set("n", "<leader>tf", ":2ToggleTerm direction=float<cr>")
