5d7a2da741e7970ab9b9e1478156ae3add394a5e
Blackhawk <daniel.peters1@pm.me>
2023-12-30 00:13:41 +0100

    Update lazygit keybinds

--- lua/config/git.lua
+++ lua/config/git.lua
@@ -3,8 +3,8 @@ require("telescope").load_extension("lazygit")
 -- lazygit bindings
 vim.keymap.set("n", "<leader>gr", "<cmd>lua require('telescope').extensions.lazygit.lazygit()<cr>")
 vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>")
-vim.keymap.set("n", "<leader>gf", "<cmd>LazyGitFilter<cr>")
-vim.keymap.set("n", "<leader>gff", "<cmd>LazyGitFilterCurrentFile<cr>")
+vim.keymap.set("n", "<leader>gh", "<cmd>LazyGitFilter<cr>")
+vim.keymap.set("n", "<leader>ghf", "<cmd>LazyGitFilterCurrentFile<cr>")
 
 -- vim-fugitive bindings
 vim.keymap.set("n", "<leader>gc", ":Gvdiffsplit<cr>")
