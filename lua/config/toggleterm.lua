70c900bdd13b9a721647083d8f246defa71d0757
Blackhawk <daniel.peters1@pm.me>
2023-12-30 00:09:16 +0100

    Fix terminal keybinds

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -1,6 +1,6 @@
 require("toggleterm").setup{
-	open_mapping = [[<leader>tt]],
 	terminal_mappings = true,
 }
 
+vim.keymap.set("n", "<leader>tt", ":1ToggleTerm direction=horizontal<cr>")
 vim.keymap.set("n", "<leader>tf", ":2ToggleTerm direction=float<cr>")
