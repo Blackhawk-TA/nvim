34c52e1603d5e4e7383fcec375650d9c6c24b27a
Blackhawk <daniel.peters1@pm.me>
2024-01-03 03:01:29 +0100

    Update trouble.nvim keybinds

--- lua/config/trouble.lua
+++ lua/config/trouble.lua
@@ -8,10 +8,10 @@ signs = {
     },
 })
 
-vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>")
-vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>")
-vim.keymap.set("n", "<leader>xf", "<cmd>TroubleToggle document_diagnostics<cr>")
-vim.keymap.set("n", "<leader>xr", "<cmd>TroubleToggle lsp_references<cr>")
-vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle lsp_definitions<cr>")
-vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>")
+vim.keymap.set("n", "<leader>zz", "<cmd>TroubleToggle<cr>")
+vim.keymap.set("n", "<leader>zw", "<cmd>TroubleToggle workspace_diagnostics<cr>")
+vim.keymap.set("n", "<leader>zf", "<cmd>TroubleToggle document_diagnostics<cr>")
+vim.keymap.set("n", "<leader>zr", "<cmd>TroubleToggle lsp_references<cr>")
+vim.keymap.set("n", "<leader>zd", "<cmd>TroubleToggle lsp_definitions<cr>")
+vim.keymap.set("n", "<leader>zq", "<cmd>TroubleToggle quickfix<cr>")
 
