d1c7d90627991629b517cfb2c8e754ff145c7b03
Blackhawk <daniel.peters1@pm.me>
2024-02-06 15:57:55 +0100

    Add keybind for toggling copilot

--- lua/config/copilot.lua
+++ lua/config/copilot.lua
@@ -8,4 +8,8 @@ require("copilot").setup({
 	suggestion = { enabled = false },
 	panel = { enabled = false },
 })
+
 require("copilot_cmp").setup()
+
+vim.keymap.set("n", "<leader>ce", "<cmd>Copilot enable<cr>")
+vim.keymap.set("n", "<leader>cd", "<cmd>Copilot disable<cr>")
