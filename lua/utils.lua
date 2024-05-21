8629455499307551ceab90ccceebe515afd79c94
Blackhawk <daniel.peters1@pm.me>
2024-05-21 18:31:59 +0200

    Replace deprecated methods

--- lua/utils.lua
+++ lua/utils.lua
@@ -8,8 +8,8 @@ local utils = {
 
 function utils.is_editable()
 	local current_buffer = vim.fn.bufnr("%")
-	local is_modifiable = vim.api.nvim_buf_get_option(current_buffer, "modifiable")
-	local is_readonly = vim.api.nvim_buf_get_option(current_buffer, "readonly")
+	local is_modifiable = vim.api.nvim_get_option_value("modifiable", { buf = current_buffer })
+	local is_readonly = vim.api.nvim_get_option_value("readonly", { buf = current_buffer })
 	return is_modifiable and not is_readonly
 end
 
