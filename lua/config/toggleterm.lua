6e12f1a47b3c4e56cf884d78586adfc8173993b3
Blackhawk <daniel.peters1@pm.me>
2024-05-28 17:50:34 +0200

    Allow multiple horizontal terminals

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -2,5 +2,11 @@ require("toggleterm").setup{
 	terminal_mappings = true,
 }
 
-vim.keymap.set("n", "<leader>tt", ":1ToggleTerm direction=horizontal<cr>")
-vim.keymap.set("n", "<leader>tf", ":2ToggleTerm direction=float<cr>")
+vim.keymap.set("n", "<leader>ta", ":ToggleTermToggleAll<cr>")
+vim.keymap.set("n", "<leader>tt", ":ToggleTerm<cr>")
+
+for i = 1, 8 do
+	vim.keymap.set("n", "<leader>t" .. i, ":" .. i .. "ToggleTerm direction=horizontal name=Term" .. i .. "<cr>")
+end
+
+vim.keymap.set("n", "<leader>tf", ":9ToggleTerm direction=float<cr>")
