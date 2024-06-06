9f0f0afdd46136235507e6ede6fd34bdac7bbf0a
Blackhawk <daniel.peters1@pm.me>
2024-06-06 17:01:45 +0200

    Update trouble keybinds

--- lua/config/trouble.lua
+++ lua/config/trouble.lua
@@ -8,9 +8,8 @@ require("trouble").setup({
 	},
 })
 
-vim.keymap.set("n", "<leader>zz", "<cmd>TroubleToggle<cr>")
-vim.keymap.set("n", "<leader>zw", "<cmd>TroubleToggle workspace_diagnostics<cr>")
-vim.keymap.set("n", "<leader>zf", "<cmd>TroubleToggle document_diagnostics<cr>")
-vim.keymap.set("n", "<leader>zr", "<cmd>TroubleToggle lsp_references<cr>")
-vim.keymap.set("n", "<leader>zd", "<cmd>TroubleToggle lsp_definitions<cr>")
-vim.keymap.set("n", "<leader>zq", "<cmd>TroubleToggle quickfix<cr>")
+vim.keymap.set("n", "<leader>zz", "<cmd>Trouble diagnostics toggle<cr>")
+vim.keymap.set("n", "<leader>zr", "<cmd>Trouble lsp_references toggle<cr>")
+vim.keymap.set("n", "<leader>zd", "<cmd>Trouble lsp_definitions toggle <cr>")
+vim.keymap.set("n", "<leader>zq", "<cmd>Trouble quickfix toggle<cr>")
+vim.keymap.set("n", "<leader>zt", "<cmd>Trouble telescope toggle<cr>")
