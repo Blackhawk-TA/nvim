5090aa314a166b9a1274b44dff74826b75c16d2d
Blackhawk <daniel.peters1@pm.me>
2024-07-02 12:37:36 +0200

    Improve copilot auto complete behaviour

--- lua/config/lsp/lsp-zero.lua
+++ lua/config/lsp/lsp-zero.lua
@@ -49,18 +49,51 @@ end)
 local cmp = require("cmp")
 local cmp_action = require("lsp-zero").cmp_action()
 
+local has_words_before = function()
+	local current_buffer = vim.fn.bufnr("%")
+	if vim.api.nvim_get_option_value("buftype", { buf = current_buffer }) == "prompt" then
+		return false
+	end
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+end
+
 cmp.setup({
 	mapping = cmp.mapping.preset.insert({
 		["<CR>"] = cmp.mapping.confirm({
 			behavior = cmp.ConfirmBehavior.Replace,
 			select = false
 		}),
-		["<Tab>"] = cmp_action.luasnip_supertab(),
+		["<Tab>"] = vim.schedule_wrap(function(fallback)
+			if cmp.visible() and has_words_before() then
+				cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+			else
+				fallback()
+			end
+		end),
 		["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-j>"] = cmp.mapping.select_next_item({ behavior = "select" }),
 		["<C-k>"] = cmp.mapping.select_prev_item({ behavior = "select" })
 	}),
+	sorting = {
+		priority_weight = 2,
+		comparators = {
+			require("copilot_cmp.comparators").prioritize,
+
+			-- Below is the default comparator list and order for nvim-cmp
+			cmp.config.compare.offset,
+			-- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+			cmp.config.compare.exact,
+			cmp.config.compare.score,
+			cmp.config.compare.recently_used,
+			cmp.config.compare.locality,
+			cmp.config.compare.kind,
+			cmp.config.compare.sort_text,
+			cmp.config.compare.length,
+			cmp.config.compare.order,
+		},
+	},
 	preselect = "item",
 	completion = {
 		completeopt = "menu,menuone,noinsert"
