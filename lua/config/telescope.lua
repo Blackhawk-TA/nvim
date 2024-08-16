d58b676ce5db1794b4f566d951b08a94457ffc45
Blackhawk <daniel.peters1@pm.me>
2024-08-16 10:32:58 +0200

    Align telescope open keybinds with neotree

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -7,7 +7,7 @@ require("telescope").setup {
 				-- map actions.which_key to <C-h> (default: <C-/>)
 				-- actions.which_key shows the mappings for your picker,
 				-- e.g. git_{create, delete, ...}_branch for the git_branches picker
-				["<C-h>"] = "which_key",
+				["<C-h>"] = "select_horizontal",
 				["<C-s>"] = "select_vertical",
 				["<C-z>"] = trouble.open
 			},
