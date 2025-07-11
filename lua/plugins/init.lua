9e3f02db6746af46b218e5904e097cd6d78fca87
Blackhawk <daniel.peters1@pm.me>
2025-07-11 13:12:48 +0200

    Support helm template syntax

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,5 +4,6 @@ return {
 	"norcalli/nvim-colorizer.lua",
 	"editorconfig/editorconfig-vim",
 	"kdheepak/lazygit.nvim",
-	"lewis6991/gitsigns.nvim"
+	"lewis6991/gitsigns.nvim",
+	"towolf/vim-helm",
 }
