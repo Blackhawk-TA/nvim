e14fc48ea32184e8ff80d3917b76144dfdd6b7b3
Blackhawk <daniel.peters1@pm.me>
2025-11-05 10:19:49 +0100

    Neotest: Prevent warning spam for parameterized go tests

--- lua/config/neotest.lua
+++ lua/config/neotest.lua
@@ -3,7 +3,9 @@ require("neotest").setup({
 		require("neotest-python")({
 			dap = { justMyCode = false },
 		}),
-		require("neotest-golang"),
+		require("neotest-golang")({
+			warn_test_name_dupes = false, -- prevent warning spam when using parameterized tests
+		}),
 	},
 })
 
