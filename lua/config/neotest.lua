0e2a9a77e4738708746635e83eb65200b46fdc81
Blackhawk <daniel.peters1@pm.me>
2026-04-10 16:10:42 +0200

    NeoTest: Lazy loading + update keymap handling

--- lua/config/neotest.lua
+++ lua/config/neotest.lua
@@ -9,32 +9,3 @@ require("neotest").setup({
 		}),
 	},
 })
-
--- Run nearest test
-vim.keymap.set("n", "<leader>uu", function()
-	vim.cmd("silent! w")
-	require("neotest").run.run()
-end)
-
--- Run current file
-vim.keymap.set("n", "<leader>uf", function()
-	vim.cmd("silent! w")
-	require("neotest").run.run(vim.fn.expand("%"))
-end)
-
--- Debug nearest test
-vim.keymap.set("n", "<leader>ud", function()
-	vim.cmd("silent! w")
-	require("neotest").run.run({ strategy = "dap" })
-end)
-
--- Stop nearest test
-vim.keymap.set("n", "<leader>U", function()
-	vim.cmd("silent! w")
-	require("neotest").run.stop()
-end)
-
--- Show test summary
-vim.keymap.set("n", "<leader>us", function()
-	vim.cmd("Neotest summary")
-end)
--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,17 +1,56 @@
 return {
-	"nvim-neotest/neotest",
-	dependencies = {
-		"nvim-neotest/nvim-nio",
-		"nvim-lua/plenary.nvim",
-		"antoinemadec/FixCursorHold.nvim",
-		"nvim-treesitter/nvim-treesitter",
-	},
-	"nvim-neotest/neotest-python",
 	{
-		"fredrikaverpil/neotest-golang",
-		version = "*",
-		build = function()
-			vim.system({ "go", "install", "gotest.tools/gotestsum@latest" }):wait()
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-neotest/neotest-python",
+			{
+				"fredrikaverpil/neotest-golang",
+				version = "*",
+				build = function()
+					vim.system({ "go", "install", "gotest.tools/gotestsum@latest" }):wait()
+				end,
+			},
+		},
+		keys = {
+			{
+				"<leader>uu",
+				function()
+					vim.cmd("silent! w")
+					require("neotest").run.run()
+				end,
+				desc = "Run Nearest Test",
+			},
+			{
+				"<leader>uf",
+				function()
+					vim.cmd("silent! w")
+					require("neotest").run.run(vim.fn.expand("%"))
+				end,
+				desc = "Run File Tests",
+			},
+			{
+				"<leader>ud",
+				function()
+					vim.cmd("silent! w")
+					require("neotest").run.run({ strategy = "dap" })
+				end,
+				desc = "Debug Nearest Test",
+			},
+			{
+				"<leader>U",
+				function()
+					vim.cmd("silent! w")
+					require("neotest").run.stop()
+				end,
+				desc = "Stop Nearest Test",
+			},
+			{ "<leader>us", "<cmd>Neotest summary<cr>", desc = "Test Summary" },
+		},
+		config = function()
+			require("config/neotest")
 		end,
 	},
 }
