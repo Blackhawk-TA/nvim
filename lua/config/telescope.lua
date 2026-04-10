37dff4c9c1d5c152818d1101d006a6ca92d8cafc
Blackhawk <daniel.peters1@pm.me>
2026-04-10 16:06:38 +0200

    Telescope: Update keymap handling

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -47,13 +47,3 @@ require("dir-telescope").setup({
 })
 require("telescope").load_extension("dir")
 require("telescope").load_extension("ui-select")
-
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-vim.keymap.set("n", "<leader>fr", builtin.lsp_references, {})
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-vim.keymap.set("n", "<leader>fy", "<cmd> Telescope neoclip extra=star,plus<CR>", {})
-
-vim.keymap.set("n", "<leader>fd", "<cmd>Telescope dir live_grep<CR>", { noremap = true, silent = true })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,10 +2,20 @@ return {
 	{
 		"nvim-telescope/telescope.nvim",
 		version = "*",
-	},
-	"princejoogie/dir-telescope.nvim",
-	"nvim-telescope/telescope-ui-select.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
+		lazy = false,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"princejoogie/dir-telescope.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
+		},
+		keys = {
+			{ "<leader>ff", function() require("telescope.builtin").find_files() end, desc = "Find Files" },
+			{ "<leader>fg", function() require("telescope.builtin").live_grep() end, desc = "Live Grep" },
+			{ "<leader>fb", function() require("telescope.builtin").buffers() end, desc = "Buffers" },
+			{ "<leader>fr", function() require("telescope.builtin").lsp_references() end, desc = "LSP References" },
+			{ "<leader>fh", function() require("telescope.builtin").help_tags() end, desc = "Help Tags" },
+			{ "<leader>fy", "<cmd>Telescope neoclip extra=star,plus<CR>", desc = "Neoclip" },
+			{ "<leader>fd", "<cmd>Telescope dir live_grep<CR>", desc = "Dir Live Grep" },
+		},
 	},
 }
