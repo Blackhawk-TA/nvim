5c6fbf0cfc0dd5a1fdeb37722e4c107bacc91efe
Blackhawk <daniel.peters1@pm.me>
2026-04-10 14:23:10 +0200

    SwaggerPreview: Update keymap handling

--- lua/config/swagger-preview.lua
+++ lua/config/swagger-preview.lua
@@ -1,22 +1,6 @@
-local system = require("utils.system")
-
 require("swagger-preview").setup({
 	-- The port to run the preview server on
 	port = 8787,
 	-- The host to run the preview server on
 	host = "localhost",
 })
-
-local preview_active = false
-
-vim.keymap.set("n", "<leader>ps", function()
-	if preview_active then
-		vim.cmd("SwaggerPreviewStop")
-		print("Stopped swagger preview server")
-		preview_active = false
-	else
-		vim.cmd("SwaggerPreview")
-		preview_active = true
-		system.open_browser("http://localhost:8787")
-	end
-end)
--- lua/plugins/swagger-preview.lua
+++ lua/plugins/swagger-preview.lua
@@ -1,6 +1,24 @@
+-- toggle_swagger_preview starts or stops the swagger preview depending on the current state
+local function toggle_swagger_preview()
+	local system = require("utils.system")
+	-- Use a global to track state across invocations
+	if vim.g.swagger_preview_active then
+		vim.cmd("SwaggerPreviewStop")
+		print("Stopped swagger preview server")
+		vim.g.swagger_preview_active = false
+	else
+		vim.cmd("SwaggerPreview")
+		vim.g.swagger_preview_active = true
+		system.open_browser("http://localhost:8787")
+	end
+end
+
 return {
 	"vinnymeller/swagger-preview.nvim",
 	cmd = { "SwaggerPreview", "SwaggerPreviewStop", "SwaggerPreviewToggle" },
 	build = "npm i",
+	keys = {
+		{ "<leader>ps", toggle_swagger_preview, desc = "Toggle Swagger Preview" },
+	},
 	config = true,
 }
