cf5a6d71e996c39a401f2b7d8ad39a26dca880f6
Blackhawk <daniel.peters1@pm.me>
2026-04-13 14:59:14 +0200

    Debugging: Add lazy loading

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -6,32 +6,29 @@ return {
 			"leoluz/nvim-dap-go",
 			"mfussenegger/nvim-dap-python",
 		},
-		keys = function ()
-			local dap = require("dap")
-			local dapui = require("dapui")
-			local debugger = require("utils.debugger")
-
-			return {
-				{ "<F5>", dap.toggle_breakpoint, desc = "Toggle Breakpoint" },
-				{ "<F6>", function() dap.set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, desc = "Conditional Breakpoint" },
-				{ "<F7>", dap.step_into, desc = "Step Into" },
-				{ "<F8>", dap.step_over, desc = "Step Over" },
-				{ "<F9>", function() debugger.start(true) end, desc = "Start Debugger" },
-				{ "<F10>", function() debugger.start(false) end, desc = "Start Debugger (no build)" },
-				{ "<F12>", dapui.eval, desc = "Eval Expression" },
-				-- Step out, equals Shift + F7
-				{ "<F19>", dap.step_out, desc = "Step Out" },
-				{ "<S-F7>", dap.step_out, desc = "Step Out" },
-				-- Re-run debugger, equals Shift + F9
-				{ "<F21>", debugger.run_last, desc = "Restart/Run Last" },
-				{ "<S-F9>", debugger.run_last, desc = "Restart/Run Last" },
-				-- Close debugger, equals Shift + F10
-				{ "<F22>", debugger.close, desc = "Close Debugger" },
-				{ "<S-F10>", debugger.close, desc = "Close Debugger" },
-				-- Stop debugging, equals Control + F9
-				{ "<F33>", function() dap.disconnect({ terminateDebuggee = true }) end, desc = "Stop Debugger" },
-				{ "<C-F9>", function() dap.disconnect({ terminateDebuggee = true }) end, desc = "Stop Debugger" },
-			}
+		keys = {
+			{ "<F5>", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+			{ "<F6>", function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, desc = "Conditional Breakpoint" },
+			{ "<F7>", function() require("dap").step_into() end, desc = "Step Into" },
+			{ "<F8>", function() require("dap").step_over() end, desc = "Step Over" },
+			{ "<F9>", function() require("utils.debugger").start(true) end, desc = "Start Debugger" },
+			{ "<F10>", function() require("utils.debugger").start(false) end, desc = "Start Debugger (no build)" },
+			{ "<F12>", function() require("dapui").eval() end, desc = "Eval Expression" },
+			-- Step out, equals Shift + F7
+			{ "<F19>", function() require("dap").step_out() end, desc = "Step Out" },
+			{ "<S-F7>", function() require("dap").step_out() end, desc = "Step Out" },
+			-- Re-run debugger, equals Shift + F9
+			{ "<F21>", function() require("utils.debugger").run_last() end, desc = "Restart/Run Last" },
+			{ "<S-F9>", function() require("utils.debugger").run_last() end, desc = "Restart/Run Last" },
+			-- Close debugger, equals Shift + F10
+			{ "<F22>", function() require("utils.debugger").close() end, desc = "Close Debugger" },
+			{ "<S-F10>", function() require("utils.debugger").close() end, desc = "Close Debugger" },
+			-- Stop debugging, equals Control + F9
+			{ "<F33>", function() require("dap").disconnect({ terminateDebuggee = true }) end, desc = "Stop Debugger" },
+			{ "<C-F9>", function() require("dap").disconnect({ terminateDebuggee = true }) end, desc = "Stop Debugger" },
+		},
+		config = function()
+			require("config/debugging")
 		end,
 	},
 	{
